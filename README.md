4f34d426ddd3e7872b819a8554986006dc0e8120
dpasse <dpasse@mpirik.com>
2022-12-28 08:44:39 -0600

    initial prefect

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # rc
 
 chrome://net-internals/#sockets
+
+https://docs.prefect.io/tutorials/first-steps/
+https://pypi.org/project/prefect/
--- app/app.py
+++ app/app.py
@@ -1,11 +1,15 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 load_dotenv()
 
+
+@app.route("/heartbeat")
+def heartbeat():
+    return jsonify({"status": "healthy"})
 
 @app.route('/', defaults={'path': ''}, methods=['GET'])
 @app.route('/<path:path>', methods=['GET'])
