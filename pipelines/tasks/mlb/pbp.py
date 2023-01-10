3c1ab775052054d0bbca9d88b365bc2f50052bad
dpasse <dpasse@mpirik.com>
2023-01-10 09:52:44 -0600

    100% lint

--- pipelines/tasks/mlb/pbp.py
+++ pipelines/tasks/mlb/pbp.py
@@ -2,122 +2,187 @@ import sys
 import time
 import json
 
-from typing import List, Optional
+from typing import List, Optional, Tuple, Dict, Any, cast
 from prefect import flow, task
-
 from common.helpers.web import make_request
 
 
-def compress_game(game: dict) -> dict:
-    if len(game['periods']):
-        first_child = game['periods'][0]
-        game.update({
-          'away': first_child['awayTeamShortName'].lower(),
-          'home': first_child['homeTeamShortName'].lower(),
-        })
+def transform_at_bat(period: Dict[str, Any]) -> Dict[str, Any]:
+    if 'atBatTeam' in period:
+        period['atBat'] = period['atBatTeam']['abbrev'].lower()
 
-        for data in game['periods']:
-            if 'atBatTeam' in data:
-                data['atBat'] = data['atBatTeam']['abbrev'].lower()
-                del data['atBatTeam']
+        del period['atBatTeam']
 
-            if 'period' in data:
-                period = data['period']
-                data['id'] = f"{period['type']}-{period['number']}".lower()
-                del data['period']
+    return period
 
-            if 'awayTeamShortName' in data:
-                del data['awayTeamShortName']
+def transform_period(period: Dict[str, Any]) -> Dict[str, Any]:
+    if 'period' in period:
+        period['id'] = f"{period['period']['type']}-{period['period']['number']}".lower()
 
-            if 'homeTeamShortName' in data:
-                del data['homeTeamShortName']
+        del period['period']
 
-            if 'dsc' in data:
-                data['plays'] = [{ 'desc': data['dsc'] }] + data['plays']
-                del data['dsc']
+    return period
 
-            if 'plays' in data:
-                data['events'] = data['plays']
-                del data['plays']
+def transform_plays(period: Dict[str, Any]) -> Dict[str, Any]:
+    if 'plays' in period:
+        period['events'] = period['plays']
 
-            data['score'] = {
-                'runs': int(data['runs']),
-                'hits': int(data['hits']),
-                'errors': int(data['errors'])
-            }
+        del period['plays']
 
-            del data['hits']
-            del data['runs']
-            del data['errors']
+    if 'dsc' in period:
+        period['events'] = [{ 'desc': period['dsc'] }] + period['events']
 
-            if 'events' in data:
-                previous_bases = None
-                for event in data['events']:
-                    if 'defaultOpen' in event:
-                        del event['defaultOpen']
+        del period['dsc']
 
-                    if 'awayScore' in event and 'homeScore' in event:
-                        event['score'] = {
-                            'away': event['awayScore'],
-                            'home': event['homeScore']
-                        }
+    return period
 
-                        del event['awayScore']
-                        del event['homeScore']
+def transform_score(period: Dict[str, Any]) -> Dict[str, Any]:
+    period['score'] = {
+        'runs': int(period['runs']),
+        'hits': int(period['hits']),
+        'errors': int(period['errors'])
+    }
 
-                    if 'isAway' in event:
-                        del event['isAway']
+    return period
 
-                    if 'id' in event:
-                        del event['id']
+def clean_period(period: Dict[str, Any]) -> Dict[str, Any]:
+    del period['hits']
+    del period['runs']
+    del period['errors']
+    del period['awayTeamShortName']
+    del period['homeTeamShortName']
 
-                    if 'dsc' in event:
-                        event['desc'] = event['dsc']
-                        del event['dsc']
+    return period
 
-                    if 'pitches' in event:
-                        for pitch in event['pitches']:
+def transform_event_score(event: Dict[str, Any]) -> Dict[str, Any]:
+    if 'awayScore' in event and 'homeScore' in event:
+        event['score'] = {
+            'away': event['awayScore'],
+            'home': event['homeScore']
+        }
 
-                            if 'id' in pitch:
-                                del pitch['id']
+        del event['awayScore']
+        del event['homeScore']
 
-                            if 'ptchCoords' in pitch:
-                                pitch['coords'] = pitch['ptchCoords']
-                                del pitch['ptchCoords']
+    return event
 
-                            if 'vlcty' in pitch:
-                                pitch['velocity'] = pitch['vlcty']
-                                del pitch['vlcty']
+def transform_event_desc(event: Dict[str, Any]) -> Dict[str, Any]:
+    if 'dsc' in event:
+        event['desc'] = event['dsc']
+        del event['dsc']
 
-                            if 'rslt' in pitch:
-                                pitch['result'] = {
-                                    'type': pitch['rslt'].lower(),
-                                }
+    return event
 
-                                if 'dsc' in pitch:
-                                    pitch['result']['outcome'] = pitch['dsc'].lower()
-                                    del pitch['dsc']
+def clean_event(event: Dict[str, Any]) -> Dict[str, Any]:
+    if 'defaultOpen' in event:
+        del event['defaultOpen']
 
-                                if 'hitCoords' in pitch:
-                                    pitch['result']['hitCoords'] = pitch['hitCoords']
-                                    del pitch['hitCoords']
+    if 'isAway' in event:
+        del event['isAway']
 
-                                del pitch['rslt']
+    if 'id' in event:
+        del event['id']
 
-                            if 'ptchDsc' in pitch:
-                                pitch['type'] = pitch['ptchDsc'].lower()
-                                del pitch['ptchDsc']
+    return event
 
-                            if 'evnts' in pitch and 'onBase' in pitch['evnts']:
-                                pitch['bases'] = [int(x) for x in pitch['evnts']['onBase']]
-                                del pitch['evnts']['onBase']
+def transform_pitch_renames(pitch: Dict[str, Any]) -> Dict[str, Any]:
+    if 'ptchCoords' in pitch:
+        pitch['coords'] = pitch['ptchCoords']
+        del pitch['ptchCoords']
 
-                                previous_bases = pitch['bases'].copy()
+    if 'vlcty' in pitch:
+        pitch['velocity'] = pitch['vlcty']
+        del pitch['vlcty']
 
-                                if len(pitch['evnts'].keys()) == 0:
-                                    del pitch['evnts']
-                            elif previous_bases:
-                                pitch['bases'] = previous_bases.copy()
+    if 'ptchDsc' in pitch:
+        pitch['type'] = pitch['ptchDsc'].lower()
+        del pitch['ptchDsc']
+
+    return pitch
+
+def transform_pitch_result(pitch: Dict[str, Any]) -> Dict[str, Any]:
+    if 'rslt' in pitch:
+        pitch['result'] = {
+            'type': pitch['rslt'].lower(),
+        }
+
+        if 'dsc' in pitch:
+            pitch['result']['outcome'] = pitch['dsc'].lower()
+            del pitch['dsc']
+
+        if 'hitCoords' in pitch:
+            pitch['result']['hitCoords'] = pitch['hitCoords']
+            del pitch['hitCoords']
+
+        del pitch['rslt']
+
+    return pitch
+
+def transform_pitch_bases_event(pitch: Dict[str, Any], previous_bases: List[int]) -> Tuple[Dict[str, Any], List[int]]:
+    if 'evnts' in pitch and 'onBase' in pitch['evnts']:
+        bases = [int(x) for x in pitch['evnts']['onBase']]
+
+        pitch['bases'] = bases
+        previous_bases = bases.copy()
+
+        del pitch['evnts']['onBase']
+
+        if len(pitch['evnts'].keys()) == 0:
+            del pitch['evnts']
+    else:
+        pitch['bases'] = previous_bases.copy()
+
+    return pitch, previous_bases
+
+def transform_clean_pitch(pitch: Dict[str, Any]) -> Dict[str, Any]:
+    if 'id' in pitch:
+        del pitch['id']
+
+    return pitch
+
+def compress_game(game: Dict[str, Any]) -> Dict[str, Any]:
+    if len(game['periods']) == 0:
+        return game
+
+    game.update({
+        'away': game['periods'][0]['awayTeamShortName'].lower(),
+        'home': game['periods'][0]['homeTeamShortName'].lower(),
+    })
+
+    for period in cast(List[Dict[str, Any]], game['periods']):
+        for transformer in (
+            transform_at_bat,
+            transform_period,
+            transform_plays,
+            transform_score,
+            clean_period,
+        ):
+            period = transformer(period)
+
+        if not 'events' in period:
+            continue
+
+        previous_bases = [0, 0, 0]
+        for event in cast(List[Dict[str, Any]], period['events']):
+            for event_transformer in (
+                transform_event_score,
+                transform_event_desc,
+                clean_event,
+            ):
+                event = event_transformer(event)
+
+            if not 'pitches' in event:
+                continue
+
+            for pitch in cast(List[Dict[str, Any]], event['pitches']):
+                for pitch_transformer in (
+                    transform_pitch_renames,
+                    transform_pitch_result,
+                    transform_clean_pitch,
+                ):
+                    pitch = pitch_transformer(pitch)
+
+                pitch, previous_bases = transform_pitch_bases_event(pitch, previous_bases)
 
     return game
 
