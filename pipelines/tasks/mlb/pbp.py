c3025e97dbe629b8730d283e7fd642bb878e0989
dpasse <dpasse@mpirik.com>
2023-01-11 09:58:20 -0600

    time series

--- pipelines/tasks/mlb/pbp.py
+++ pipelines/tasks/mlb/pbp.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import time
 import json
@@ -85,6 +86,20 @@ def clean_event(event: Dict[str, Any]) -> Dict[str, Any]:
 
     return event
 
+def expand_info_play_event(event: Dict[str, Any]) -> Dict[str, Any]:
+    should_expand_if = 'isInfoPlay' in event or 'isPitcherChange' in event
+    if not should_expand_if:
+        return event
+
+    sub_match = re.search(r'^(.+?) (hit for|in|as|catching|pitching)', event['desc'])
+    if sub_match:
+        event['player'] = sub_match.group(1)
+        event['type'] = 'sub-p' if sub_match.group(2) == 'pitching' else 'sub-f'
+    else:
+        event['type'] = 'after-pitch'
+
+    return event
+
 def transform_pitch_renames(pitch: Dict[str, Any]) -> Dict[str, Any]:
     if 'ptchCoords' in pitch:
         pitch['coords'] = pitch['ptchCoords']
@@ -97,6 +112,10 @@ def transform_pitch_renames(pitch: Dict[str, Any]) -> Dict[str, Any]:
     if 'ptchDsc' in pitch:
         pitch['type'] = pitch['ptchDsc'].lower()
         del pitch['ptchDsc']
+
+    if 'count' in pitch:
+        pitch['order'] = pitch['count']
+        del pitch['count']
 
     return pitch
 
@@ -115,6 +134,8 @@ def transform_pitch_result(pitch: Dict[str, Any]) -> Dict[str, Any]:
             del pitch['hitCoords']
 
         del pitch['rslt']
+    else:
+        pitch['result'] = {}
 
     return pitch
 
@@ -122,7 +143,7 @@ def transform_pitch_bases_event(pitch: Dict[str, Any], previous_bases: List[int]
     if 'evnts' in pitch and 'onBase' in pitch['evnts']:
         bases = [int(x) for x in pitch['evnts']['onBase']]
 
-        pitch['bases'] = bases
+        pitch['result']['bases'] = bases
         previous_bases = bases.copy()
 
         del pitch['evnts']['onBase']
@@ -130,15 +151,53 @@ def transform_pitch_bases_event(pitch: Dict[str, Any], previous_bases: List[int]
         if len(pitch['evnts'].keys()) == 0:
             del pitch['evnts']
     else:
-        pitch['bases'] = previous_bases.copy()
+        pitch['result']['bases'] = previous_bases.copy()
 
     return pitch, previous_bases
+
+def transform_pitch_set_count(pitch: Dict[str, Any], count: dict) -> Tuple[Dict[str, Any], dict]:
+    pitch['count'] = count.copy()
+    if pitch['result']['type'] == 'strike' or (count['strikes'] < 2 and pitch['result']['type'] == 'foul'):
+        count['strikes'] += 1
+
+    count['balls'] += int(pitch['result']['type'] == 'ball')
+
+    return pitch, count
 
 def transform_clean_pitch(pitch: Dict[str, Any]) -> Dict[str, Any]:
     if 'id' in pitch:
         del pitch['id']
 
     return pitch
+
+def tie_after_pitch_events_to_pitch(events: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+    after_pitch_events: List[Dict[str, Any]] = []
+    for event in events:
+        if 'type' in event:
+            if event['type'] == 'after-pitch':
+                after_pitch_events.append(event)
+        elif len(after_pitch_events) > 1:
+            for pitch in cast(List[Dict[str, Any]], event['pitches']):
+                if not 'action' in pitch['result']:
+                    continue
+
+                after_pitch_event, after_pitch_events = after_pitch_events[0], after_pitch_events[1:]
+
+                after_pitch_event['after-pitch-event'] = {
+                    'id': event['id'],
+                    'pitch': pitch['order']
+                }
+
+                pitch['result']['after-pitch-event'] = after_pitch_event['id']
+
+            if len(after_pitch_events) > 0:
+                print()
+                print('NOT ALL "after-pitch" EVENTS COULD BE ACCOUNTED FOR!!')
+                print()
+
+            after_pitch_events.clear()
+
+    return events
 
 def compress_game(game: Dict[str, Any]) -> Dict[str, Any]:
     if len(game['periods']) == 0:
@@ -164,11 +223,13 @@ def compress_game(game: Dict[str, Any]) -> Dict[str, Any]:
             continue
 
         previous_bases = [0, 0, 0]
-        for event in cast(List[Dict[str, Any]], period['events']):
+        events = cast(List[Dict[str, Any]], period['events'])
+        for event in events:
             for event_transformer in (
                 transform_event_score,
                 transform_event_desc,
                 clean_event,
+                expand_info_play_event,
             ):
                 event = event_transformer(event)
 
@@ -178,7 +239,13 @@ def compress_game(game: Dict[str, Any]) -> Dict[str, Any]:
             if not 'pitches' in event:
                 continue
 
-            for pitch in cast(List[Dict[str, Any]], event['pitches']):
+            count = {
+                'balls': 0,
+                'strikes': 0,
+            }
+
+            pitches = cast(List[Dict[str, Any]], event['pitches'])
+            for pitch in pitches:
                 for pitch_transformer in (
                     transform_pitch_renames,
                     transform_pitch_result,
@@ -187,6 +254,16 @@ def compress_game(game: Dict[str, Any]) -> Dict[str, Any]:
                     pitch = pitch_transformer(pitch)
 
                 pitch, previous_bases = transform_pitch_bases_event(pitch, previous_bases)
+                pitch, count = transform_pitch_set_count(pitch, count)
+
+            last_pitch = pitches[0]
+            for current_pitch in pitches[1:-1]:
+                if last_pitch['result']['bases'] != current_pitch['result']['bases']:
+                    last_pitch['result']['action'] = True
+
+                last_pitch = current_pitch
+
+        events = tie_after_pitch_events_to_pitch(events)
 
     return game
 
